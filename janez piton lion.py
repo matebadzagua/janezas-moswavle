634dcff2cf3e0ec0bb878025b33ed8db9549739a
matebadzagua <146456473+matebadzagua@users.noreply.github.com>
2023-11-02 05:42:49 -0700

    Add files via upload

---
+++ janez piton lion.py
@@ -0,0 +1,11 @@
+def samkutxedis_katetebi ( A , B , C ) :
+
+    if (A+B) < c :
+
+      print ("ასეთი სამკუტხედი არ იქნება")  
+    elif ( A+B ) >  c :
+
+      print ("ასეთი  სამკუტხედი იქნება")
+  
+  
+    samkutxedis_katetebi (  5 ,  6 ,  9 )
